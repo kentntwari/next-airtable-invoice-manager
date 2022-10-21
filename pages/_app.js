8121e6341c701951b02755f59bcdbdb0b6726fd8
Kent Ntwari <kent.ntwari@ncrfs.com>
2022-10-21 16:54:00 -0400

    initializing airtable in app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="min-h-screen bg-white-lighter dark:bg-blue-dark">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,56 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: '0.688rem',
+      base: '0.75rem',
+      lg: '1rem',
+      xl: '1.25rem',
+      '2xl': '2rem',
+    },
+    lineHeight: {
+      15: '0.938rem',
+      18: '1.125rem',
+      22: '1.375rem',
+      24: '1.5rem',
+      36: '2.25rem',
+    },
+    letterSpacing: {
+      'tight-4': '-1px',
+      'tight-3': '-0.8px',
+      'tight-2': '-0.63px',
+      'tight-1': '-0.25px',
+      tight: '-0.23px',
+      normal: 0,
+    },
+    extend: {
+      colors: {
+        violet: {
+          light: '#9277ff',
+          normal: '#7c5dfa',
+        },
+        blue: {
+          lightest: '#dfe3fa',
+          lighter: '#888ebo',
+          light: '#7e88c3',
+          strong: '#252945',
+          dark: '#141625',
+          darker: '#1e2139',
+        },
+        black: {
+          shade: '#0c0e16',
+        },
+        red: {
+          lighter: '#ff9797',
+          light: '#ec5757',
+        },
+        white: {
+          lighter: '#f8f8fb',
+          light: '#f9fafe',
+          full: '#ffffff',
+        },
+      },
+    },
   },
   plugins: [],
 };
