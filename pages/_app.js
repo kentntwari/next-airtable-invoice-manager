2081afadb0ad0b90a6672bf24a475ef74c9e3797
Kent Ntwari <kent.ntwari@ncrfs.com>
2022-10-24 17:00:52 -0400

    wrapped app with layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import Layout from '../components/Layout';
+
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="min-h-screen bg-white-lighter dark:bg-blue-dark">
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </div>
   );
 }
