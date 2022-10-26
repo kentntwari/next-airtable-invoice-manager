c5a7db972b409eab5274669a4f08a93c27ae40df
Kent Ntwari <kent.ntwari@ncrfs.com>
2022-10-26 16:55:19 -0400

    added tailwind classes to main

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <NavBar />
-      <main>{children}</main>
+      <main className="px-6 flex flex-col gap-8">{children}</main>
     </>
   );
 };
