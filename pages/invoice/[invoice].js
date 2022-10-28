ce4f3cde45abdfc291c8fc2492effbf689ccc8d8
Kent Ntwari <kent.ntwari@ncrfs.com>
2022-10-28 10:07:58 -0400

    set invoice page

--- pages/invoice/[invoice].js
+++ pages/invoice/[invoice].js
@@ -1,20 +1,58 @@
-import { getClientsTable } from '../../lib/airtable';
+import Link from 'next/link';
 
-const Invoice = ({ data }) => {
-  console.log(data);
-  return <div>hello</div>;
+import InvoiceContext from '../../context/InvoiceContext';
+import InvoiceDetails from '../../modules/invoice/InvoiceDetails';
+
+import { getClientsTable, getProductsTable } from '../../lib/airtable';
+
+const Invoice = ({ client, products }) => {
+  return (
+    <>
+      <InvoiceContext.Provider
+        value={{
+          client: client[0],
+          products,
+        }}>
+        <Link href="/">
+          <a className="flex items-center gap-6">
+            <span>
+              <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M6.342.886L2.114 5.114l4.228 4.228"
+                  stroke="#9277FF"
+                  strokeWidth="2"
+                  fill="none"
+                  fillRule="evenodd"
+                />
+              </svg>
+            </span>
+
+            <span className="font-bold body--1 text-black-shade">Go back</span>
+          </a>
+        </Link>
+
+        <InvoiceDetails />
+      </InvoiceContext.Provider>
+    </>
+  );
 };
 
 export const getServerSideProps = async (ctx) => {
-  const records = await getClientsTable();
+  const clientRecords = await getClientsTable();
+  const productRecords = await getProductsTable();
 
-  const filtered = JSON.parse(JSON.stringify(records))
+  const filteredClientRecords = JSON.parse(JSON.stringify(clientRecords))
+    .map((record) => record.fields)
+    .filter(({ invoiced }) => invoiced[0] === ctx.query.invoice);
+
+  const filteredProductRecords = JSON.parse(JSON.stringify(productRecords))
     .map((record) => record.fields)
     .filter(({ invoiced }) => invoiced[0] === ctx.query.invoice);
 
   return {
     props: {
-      data: filtered,
+      client: filteredClientRecords,
+      products: filteredProductRecords,
     },
   };
 };
