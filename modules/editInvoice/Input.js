2022db50488cb69d34f24bdf2872320ffd905574
Kent Ntwari <designerjehovah@gmail.com>
2022-10-31 07:11:47 -0400

    wrapped component in forward ref

--- modules/editInvoice/Input.js
+++ modules/editInvoice/Input.js
@@ -1,45 +1,53 @@
-const Input = ({
-  type = 'text',
-  id,
-  name = null,
-  defaultValue,
-  addComponentClass = null,
-  addLabelClass = null,
-  addInputClass = null,
-  labelled,
-  readOnly = false,
-}) => {
-  const defaultComponentClass = 'flex flex-col gap-2.5';
-  const defaultLabelClass = 'body--1 text-blue-light';
-  const defaultInputClass =
-    'w-full px-5 py-4 rounded border focus:border-blue-lightest outline-none';
+import React from 'react';
 
-  return (
-    <div
-      className={
-        !addComponentClass
-          ? defaultComponentClass
-          : defaultComponentClass + ' ' + addComponentClass
-      }>
-      <label
-        htmlFor={id}
+const Input = React.forwardRef(
+  (
+    {
+      type = 'text',
+      id,
+      name = null,
+      defaultValue,
+      addComponentClass = null,
+      addLabelClass = null,
+      addInputClass = null,
+      labelled,
+      readOnly = false,
+    },
+    ref
+  ) => {
+    const defaultComponentClass = 'flex flex-col gap-2.5';
+    const defaultLabelClass = 'body--1 text-blue-light';
+    const defaultInputClass =
+      'w-full px-5 py-4 rounded border focus:border-blue-lightest outline-none';
+
+    return (
+      <div
         className={
-          !addLabelClass ? defaultLabelClass : defaultLabelClass + ' ' + addLabelClass
+          !addComponentClass
+            ? defaultComponentClass
+            : defaultComponentClass + ' ' + addComponentClass
         }>
-        {labelled}
-      </label>
-      <input
-        type={type}
-        id={id}
-        name={name ? name : id}
-        defaultValue={defaultValue || null}
-        className={
-          !addInputClass ? defaultInputClass : defaultInputClass + ' ' + addInputClass
-        }
-        readOnly={readOnly}
-      />
-    </div>
-  );
-};
+        <label
+          htmlFor={id}
+          className={
+            !addLabelClass ? defaultLabelClass : defaultLabelClass + ' ' + addLabelClass
+          }>
+          {labelled}
+        </label>
+        <input
+          ref={ref}
+          type={type}
+          id={id}
+          name={name ? name : id}
+          defaultValue={defaultValue || null}
+          className={
+            !addInputClass ? defaultInputClass : defaultInputClass + ' ' + addInputClass
+          }
+          readOnly={readOnly}
+        />
+      </div>
+    );
+  }
+);
 
 export default Input;
