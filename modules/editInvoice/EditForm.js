60b9c99973bd19924e777ef84ff285c136cc889d
Kent Ntwari <designerjehovah@gmail.com>
2022-10-31 07:10:24 -0400

    wrapped ItemList in context with reducer methods

--- modules/editInvoice/EditForm.js
+++ modules/editInvoice/EditForm.js
@@ -1,6 +1,7 @@
-import { useContext } from 'react';
+import { useContext, useReducer, useEffect } from 'react';
 
 import InvoiceContext from '../../context/InvoiceContext';
+import ItemListContext from '../../context/ItemListContext';
 
 import ItemList from './ItemList';
 import SenderDetails from './SenderDetails';
@@ -9,9 +10,29 @@ import InvoiceDetails from './InvoiceDetails';
 import * as Button from '../../components/Button';
 
 const EditForm = () => {
-  const { client } = useContext(InvoiceContext);
+  const { client, products } = useContext(InvoiceContext);
 
   const { invoice_id } = client;
+
+  let mountedState = {
+    listItems: products,
+    deleteItems: [],
+  };
+
+  const [state, dispatch] = useReducer(reducer, mountedState);
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case 'REMOVE_ITEM':
+        return {
+          ...state,
+          listItems: action.payload.updateList,
+        };
+
+        case 'UPDATE_INVOICE':
+          
+    }
+  }
 
   return (
     <div className="min-h-screen w-full absolute top-0 left-0 bg-white-full flex flex-col gap-6">
@@ -31,11 +52,18 @@ const EditForm = () => {
           <ClientDetails />
         </div>
         <InvoiceDetails />
-        <ItemList />
+        <ItemListContext.Provider
+          value={{
+            list: state.listItems,
+            triggerItemDispatch: dispatch,
+            itemState: state,
+          }}>
+          <ItemList />
+        </ItemListContext.Provider>
 
         <div className="px-6 py-5 flex justify-end items-center gap-2">
           <Button.Edit>Cancel</Button.Edit>
-          <Button.Default>Save Changes</Button.Default>
+          <Button.Default type="submit">Save Changes</Button.Default>
         </div>
       </form>
     </div>
