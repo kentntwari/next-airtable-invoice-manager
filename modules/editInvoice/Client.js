2a00a67c58d45c4d9ca566dfe8656d06aa586d18
Kent Ntwari <kent.ntwari@ncrfs.com>
2022-11-09 11:49:50 -0500

    added callback attribute to component

--- modules/editInvoice/Client.js
+++ modules/editInvoice/Client.js
@@ -5,7 +5,91 @@ import FormContext from '../../context/FormContext';
 import Input from './Input';
 
 const Client = () => {
-  const { client } = useContext(FormContext);
+  const { client, state, dispatch } = useContext(FormContext);
+
+  const { name, email, address } = client;
+
+  function updateState(e) {
+    switch (e.target.name) {
+      case 'client_name':
+        dispatch({
+          type: 'UPDATE_CLIENT_INFO',
+          payload: {
+            ...state.client,
+            name: e.target.value,
+          },
+        });
+
+        break;
+
+      case 'client_email':
+        dispatch({
+          type: 'UPDATE_CLIENT_INFO',
+          payload: {
+            ...state.client,
+            email: e.target.value,
+          },
+        });
+
+        break;
+
+      case 'client_address_street':
+        dispatch({
+          type: 'UPDATE_CLIENT_INFO',
+          payload: {
+            ...state.client,
+            address: {
+              ...state.client.address,
+              street: e.target.value,
+            },
+          },
+        });
+
+        break;
+
+      case 'client_address_city':
+        dispatch({
+          type: 'UPDATE_CLIENT_INFO',
+          payload: {
+            ...state.client,
+            address: {
+              ...state.client.address,
+              city: e.target.value,
+            },
+          },
+        });
+
+        break;
+
+      case 'client_address_post_code':
+        dispatch({
+          type: 'UPDATE_CLIENT_INFO',
+          payload: {
+            ...state.client,
+            address: {
+              ...state.client.address,
+              post_code: e.target.value,
+            },
+          },
+        });
+
+        break;
+
+      case 'client_address_country':
+        dispatch({
+          type: 'UPDATE_CLIENT_INFO',
+          payload: {
+            ...state.client,
+            address: {
+              ...state.client.address,
+              country: e.target.value,
+            },
+          },
+        });
+
+        break;
+    }
+  }
 
   return (
     <Fragment>
@@ -13,31 +97,41 @@ const Client = () => {
         id="client_name"
         name="client_name"
         labelled="Client's Name"
-        defaultValue={client.name}
+        defaultValue={name}
+        callBack={(e) => updateState(e)}
       />
       <Input
         type="email"
         id="client_email"
         labelled="Client's Email"
-        defaultValue={client.email}
+        defaultValue={email}
+        callBack={(e) => updateState(e)}
       />
       <Input
         id="client_address_street"
         labelled="Street Address"
-        defaultValue={client.address.street}
+        defaultValue={address.street}
+        callBack={(e) => updateState(e)}
       />
       <div className="flex justify-between items-center gap-6">
-        <Input id="client_city" labelled="City" defaultValue={client.address.city} />
+        <Input
+          id="client_city"
+          labelled="City"
+          defaultValue={client.address.city}
+          callBack={(e) => updateState(e)}
+        />
         <Input
           id="client_address_post_code"
           labelled="Post Code"
-          defaultValue={client.address.post_code}
+          defaultValue={address.post_code}
+          callBack={(e) => updateState(e)}
         />
       </div>
       <Input
         id="client_address_country"
         labelled="Country"
-        defaultValue={client.address.country}
+        defaultValue={address.country}
+        callBack={(e) => updateState(e)}
       />
     </Fragment>
   );
